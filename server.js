c65e4a6f3c0f8cddc970b363eafdfd1ec7f5e534
JagadeeshChittula <chittulaj@gmail.com>
2026-05-18 11:22:37 +0530

    updated route

--- server.js
+++ server.js
@@ -5,33 +5,33 @@ const app=express();
 const bcrypt = require('bcrypt');
 
 
-const user1={
-    name:"chittaranjan",
-    age:22,
-    city:"Bhubaneswar"
-}
+// const user1={
+//     name:"chittaranjan",
+//     age:22,
+//     city:"Bhubaneswar"
+// }
 
-const data={
-    message: "Hii this is a example"
-};
-
-
+// const data={
+//     message: "Hii this is a example"
+// };
 
 
-app.get("/data",(req,res)=>{
-    res.send(data);
-});
 
-app.get("/user", (req, res) => {``
-    res.json(user);
-});
 
-app.get("/",(req,res)=>{
-    // res.json("<h2>Welcome to my server</h2>");
-    // res.send("<h2>Welcome to my server</h2>");
-    res.end("<h2>Welcome to my server</h2>");
+// app.get("/data",(req,res)=>{
+//     res.send(data);
+// });
 
-})
+// app.get("/user", (req, res) => {``
+//     res.json(user);
+// });
+
+// app.get("/",(req,res)=>{
+//     // res.json("<h2>Welcome to my server</h2>");
+//     // res.send("<h2>Welcome to my server</h2>");
+//     res.end("<h2>Welcome to my server</h2>");
+
+// })
 
 
 
